74412c1227f84d2fddd5b64abe7189f20be3154f
Данилецкий Артём Андреевич <412661@kali.by>
2023-04-13 16:35:25 +0300

    fix

--- src/ElasticsearchHandler.php
+++ src/ElasticsearchHandler.php
@@ -4,31 +4,23 @@ namespace Elastic;
 
 use Elasticsearch\ClientBuilder;
 use Monolog\Handler\ElasticsearchHandler as Handler;
+use Monolog\Level;
+use Elastic\Elasticsearch\Client as Client8;
 
 class ElasticsearchHandler extends Handler
 {
-    
-    /**
-     * @param Client|Client8 $client  Elasticsearch Client object
-     * @param mixed[]        $options Handler configuration
-     *
-     * @phpstan-param InputOptions $options
-     */
-    public function __construct(Client|Client8 $client, array $options = [], int|string|Level $level = Level::Debug, bool $bubble = true)
+    public function __construct()
     {
-        parent::__construct($level, $bubble);
         $this->client = ClientBuilder::create()->setHosts([env('ELASTIC_HOST', "http://elasticsearch:9200/")])->build();
-        $this->options = array_merge(
+        $this->options = 
             [
                 'index' => env('ELASTIC_LOGS_INDEX', 'default'),
                 'type'  => '_doc',
                 'ignore_error' => true,    // Suppress Elasticsearch exceptions
                 'op_type'      => 'index',   // Elastic op_type (index or create) (https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html#docs-index-api-op_type)
-            ],
-            $options
-        );
+            ];
 
-        if ($client instanceof Client8 || $client::VERSION[0] === '7') {
+        if ($this->client instanceof Client8 || $this->client::VERSION[0] === '7') {
             $this->needsType = false;
             // force the type to _doc for ES8/ES7
             $this->options['type'] = '_doc';
