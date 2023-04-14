1fb2a8daae151ff1549a8fa66689bf89a2a80dce
Данилецкий Артём Андреевич <412661@kali.by>
2023-04-14 09:33:35 +0300

    fix

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -9,14 +9,8 @@ use Monolog\Handler\AbstractHandler as Handler;
 abstract class AbstractHandler extends Handler
 {
     public static function boot(int|string|Level $level = Level::Debug, bool $bubble = true) {
-        self::setLevel($level);
+        self::$level = Logger::toMonologLevel($level);
         self::$bubble = $bubble;
     }
-
-    public static function setLevel(int|string|Level $level): self
-    {
-        self::$level = Logger::toMonologLevel($level);
-
-        return self;
-    }
+    
 }
